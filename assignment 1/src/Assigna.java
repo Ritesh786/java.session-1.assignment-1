0d752399463368bb8bc4e255bf9f81d7e94a04a9
Ritesh786 <jharitesh6@gmail.com>
2016-07-13 13:55:04 +0530

    first github commit
    first github commit

---
+++ assignment 1/src/Assigna.java
@@ -0,0 +1,13 @@
+
+public class Assigna {
+
+	public static void main(String[] args) {
+		int a=10;
+		float b=20.0f;
+		 int c=(int)(a+b);
+		 System.out.println("add of two no. is "+ c);
+		 
+
+	}
+
+}
